ccbfb2e9db0cbe5b9a31ef3eef99f7bf15a44738
David Brungardt <34960989+brungardtdb@users.noreply.github.com>
2018-02-03 18:21:42 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Law_of_Sines_Calculator
+{
+    class Program
+    {
+        static void Main()
+        {
+
+
+
+            TriangleSide triangleSide1 = new TriangleSide();
+            // Angle 1
+          
+                Console.WriteLine("Please enter the known angle in degrees (decimal) of the oblique triangle");  // Get the first known angle from the user.
+                string knownAngle1 = Console.ReadLine();
+                triangleSide1.KnownAngle1 = Convert.ToDouble(knownAngle1);
+         
+
+            double knownAngleRadians = (triangleSide1.KnownAngle1 * Math.PI) / 180; // Convert angle (degrees) into radians.
+                triangleSide1.KnownAngleRadians1 = knownAngleRadians;
+
+                double knownAngleSine = Math.Sin(triangleSide1.KnownAngleRadians1); // Convert angle radians into Sine
+                triangleSide1.KnownAngleSine1 = knownAngleSine;
+
+                Console.WriteLine($"The sine of the angle is: {triangleSide1.KnownAngleSine1}");
+         
+
+            // Side 1
+            Console.WriteLine("Please enter the length of the side opposite to the first known angle"); // Get side opposite to first known angle
+            string knownSide1 = Console.ReadLine();
+            triangleSide1.KnownSide1 = Convert.ToDouble(knownSide1);
+
+            // Angle 2
+            Console.WriteLine("Please enter the known angle in degrees (decimal) of the oblique triangle, opposite to the unknown side: "); // Get the second known angle from the user.
+            string knownAngle2 = Console.ReadLine();
+            triangleSide1.KnownAngle2 = Convert.ToDouble(knownAngle2);
+
+            double knownAngleRadians2 = (triangleSide1.KnownAngle2 * Math.PI) / 180; // Convert angle (degrees) into radians.
+            triangleSide1.KnownAngleRadians2 = knownAngleRadians2;
+
+            double knownAngleSine2 = Math.Sin(triangleSide1.KnownAngleRadians2); // Convert angle radians into Sine
+            triangleSide1.KnownAngleSine2 = Convert.ToDouble(knownAngleSine2);
+
+            Console.WriteLine($"The sine of the second angle is: {triangleSide1.KnownAngleSine2}");
+
+
+            // Unknown Side
+            double uknownSide = ((triangleSide1.KnownSide1 * triangleSide1.KnownAngleSine2) / knownAngleSine); // Calculate unknown side using law of sines: sin(A) / A = sin(B) / B = sin(C) / C
+            triangleSide1.UnKnownSide1 = Convert.ToDouble(uknownSide);
+
+            Console.WriteLine($"\nThe length of the unknown side is: {triangleSide1.UnKnownSide1}");
+
+        }
+    }
+
+}
---
+++ Triangle.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    class TriangleSide
+    {
+
+    private double _knownAngle1; // instance variable
+
+    // The known angle the user will enter 
+    public double KnownAngle1
+    {
+        get
+        {
+            return _knownAngle1;
+        }
+        set
+        {
+            _knownAngle1 = value;
+        }
+    }
+
+    private double _knownSide1; // instance variable
+
+    // The length of the side that corresponds with the known angle
+    // (The side opposite of the known angle)
+    public double KnownSide1
+    {
+        get
+        {
+            return _knownSide1;
+        }
+        set
+        {
+            _knownSide1 = value;
+        }
+    }
+
+    private double _knownAngleSine1; // instance variable
+
+    // The sine of the known angle that the user has entered
+    public double KnownAngleSine1
+    {
+        get
+        {
+            return _knownAngleSine1;
+        }
+        set
+        {
+            _knownAngleSine1 = value;
+        }
+    }
+
+    private double _knownAngleRadians1; // instance variable
+
+    // the known angle converted from degrees to radians.
+    public double KnownAngleRadians1
+    {
+        get
+        {
+            return _knownAngleRadians1;
+        }
+        set
+        {
+            _knownAngleRadians1 = value; 
+        }
+    }
+
+    private double _knownAngle2; // instance variable
+
+    // The known angle the user will enter 
+    public double KnownAngle2
+    {
+        get
+        {
+            return _knownAngle2;
+        }
+        set
+        {
+            _knownAngle2 = value;
+        }
+    }
+
+    private double _unknownSide1; // instance variable
+
+    // The length of the side that corresponds with the known angle
+    // (The side opposite of the known angle)
+    public double UnKnownSide1
+    {
+        get
+        {
+            return _unknownSide1;
+        }
+        set
+        {
+            _unknownSide1 = value;
+        }
+    }
+
+    private double _knownAngleSine2; // instance variable
+
+    // The sine of the known angle that the user has entered
+    public double KnownAngleSine2
+    {
+        get
+        {
+            return _knownAngleSine2;
+        }
+        set
+        {
+            _knownAngleSine2 = value;
+        }
+    }
+
+    private double _knownAngleRadians2; // instance variable
+
+    // the known angle converted from degrees to radians.
+    public double KnownAngleRadians2
+    {
+        get
+        {
+            return _knownAngleRadians2;
+        }
+        set
+        {
+            _knownAngleRadians2 = value;
+        }
+    }
+
+
+}
+
+
